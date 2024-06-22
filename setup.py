ee26098ee8fa004894ac89ae70529502ece8b8b5
EdexCode <edexcode@gmail.com>
2024-06-23 00:33:51 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['random'],
+    install_requires=[],
     keywords=['python', 'bot', 'chatbot', 'botdeveloper', 'predefined'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
